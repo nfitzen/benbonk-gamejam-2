d2d0174a74d91917bcab8a5ae1077a9ea8dba9ef
Nathaniel F <nathaniel.fitzenrider@gmail.com>
2020-11-23 14:44:42 -0500

    Move randomize() to TitleScreen

---
+++ project/assets/scripts/OnRun.gd
@@ -0,0 +1,8 @@
+# SPDX-FileCopyrightText: 2020 Henry Schneider, Kai Hoop, Nathaniel Fitzenrider, and Austin Chang
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+extends Node
+
+func _init():
+    randomize()
