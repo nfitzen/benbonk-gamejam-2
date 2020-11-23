31cf994104b00ec9c526ead9c47e2745afe2a851
Kai Hoop <unrelatedstring@gmail.com>
2020-11-23 00:06:42 -0500

    spin attackS

--- project/assets/entities/Enemies/Goon.gd
+++ project/assets/entities/Enemies/Goon.gd
@@ -1,7 +1,10 @@
 extends KinematicBody2D
 
-export var speed = 15
+export var speed = 12
 var engage_range = 20
+var attacking = false
+var attack_timer
+export var attack_speed = .8
 
 # tim
 func _ready():
@@ -11,7 +14,14 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
     var target = $"../Player".position
-    if (target - position).length() > engage_range:
-        move_and_slide(Vector2(-speed, 0).rotated(position.angle_to_point(target)))
+    if not attacking:
+        if (target - position).length() > engage_range:
+            move_and_slide(Vector2(-speed, 0).rotated(position.angle_to_point(target)))
+        else:
+            attacking = true
+            attack_timer = 0
     else:
-        rotate(2)
+        attack_timer += delta
+        if attack_timer >= attack_speed:
+            rotate(2)
+            attacking = false
